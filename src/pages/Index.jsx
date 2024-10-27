9370deea7797da71668f0db903908a11eb9695dc
JULIO PASCHOAL <juliocpaschoal@gmail.com>
2024-10-27 11:01:58 -0300

    estilizando pagina de tweet

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,5 +1,6 @@
 import TextInput from "../components/TextInput";
 import { useState } from 'react';
+import styles from './Index.module.css';
 
 export default function Index() {
   const [text, setText] = useState('');
@@ -20,9 +21,9 @@ export default function Index() {
 
   return (
     <div>
-    <h1>Treina Tweet</h1>
-    <div>
-      <img src={'http://github.com/juliopaschoal.png'}/>
+    <h1 className={styles.pageTitle}>TREINA TWEET</h1>
+    <div className={styles.tweetContainer}>
+      <img className={styles.avatar} src={'http://github.com/juliopaschoal.png'}/>
       <TextInput
         placeholder={'O que está acontecendo'}
         maxLength={maxLength}
@@ -30,9 +31,9 @@ export default function Index() {
         onChange={onTextChange}
       />
     </div>
-    <div>
+    <div className={styles.buttonContainer}>
       <div>{text.length} / {maxLength}</div>
-      <button onClick={sentTweet}>Tweetar</button>
+      <button className={styles.postButton} onClick={sentTweet}>Tweetar</button>
     </div>
     <ul>
         {tweeList.map(twee => {
